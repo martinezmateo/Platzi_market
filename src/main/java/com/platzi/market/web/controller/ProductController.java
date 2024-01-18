b2a755c2100694152fd237cf6d2cfbd70f2970f8
XvenusD <zxdmateozxd@gmail.com>
2024-01-18 12:11:19 -0500

    first commit

--- src/main/java/com/platzi/market/web/controller/ProductController.java
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -1,12 +1,6 @@
 package com.platzi.market.web.controller;
-
 import com.platzi.market.domain.Product;
 import com.platzi.market.domain.service.ProductService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,20 +16,12 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
-    @ApiOperation(value = "Get all supermarket products", authorizations = { @Authorization(value="JWT") })
-    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Product>> getAll() {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    @ApiOperation(value = "Search a product with an ID", authorizations = { @Authorization(value="JWT") })
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 404, message = "Product not found"),
-    })
-    public ResponseEntity<Product> getProduct(@ApiParam(value = "The id of the product", required = true, example = "7")
-                                                  @PathVariable("id") int productId) {
+    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
